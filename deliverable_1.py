62413192765f45c068281479479086683534cb21
Dimitris Papadimitriou <jim_pap@live.com>
2024-05-14 19:50:07 +0300

    some testing with edge detection

--- deliverable_1.py
+++ deliverable_1.py
@@ -23,9 +23,17 @@ def my_hough_transform(
     pass
     
 
-filepath = "images/im2.jpg"
-img = cv2.imread(filepath,cv2.IMREAD_COLOR)
-rgb_img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+filepath = "images/im5.jpg"
+img_bgr = cv2.imread(filepath,cv2.IMREAD_COLOR)
+img_rgb = cv2.cvtColor(img_bgr,cv2.COLOR_BGR2RGB)
+img_gs = cv2.cvtColor(img_bgr,cv2.COLOR_BGR2GRAY)
+img_blur = cv2.GaussianBlur(img_gs,(31,31),sigmaX=0,sigmaY=0)
+img_sobel = cv2.Sobel(img_blur,ddepth=-1,dx=1,dy=1,ksize=5)
+img_canny = cv2.Canny(img_blur, threshold1=0,threshold2=50)
 plot.figure()
-plot.imshow(rgb_img)
+plot.imshow(img_gs,cmap='gray')
+plot.figure()
+plot.imshow(img_sobel,cmap='gray')
+plot.figure()
+plot.imshow(img_canny,cmap='gray')
 plot.show()
